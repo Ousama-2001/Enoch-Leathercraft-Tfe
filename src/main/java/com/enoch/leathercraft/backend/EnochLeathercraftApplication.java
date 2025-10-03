545672b3f106b803e3e533998fadfbe565b08d9d
ousama <ousama850@gmail.com>
2025-10-03 09:34:00 +0200

    test first commit

---
+++ src/main/java/com/enoch/leathercraft/backend/EnochLeathercraftApplication.java
@@ -0,0 +1,13 @@
+package com.enoch.leathercraft.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnochLeathercraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnochLeathercraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enoch/leathercraft/backend/EnochLeathercraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enoch.leathercraft.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnochLeathercraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
